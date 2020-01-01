5f348ce6130ae97b6800c5e0b484fb690c95533f
ericcapiz <53915408+ericcapiz@users.noreply.github.com>
2020-01-01 10:08:04 -0600

    box movement complete

--- public/js/dist/main.js
+++ public/js/dist/main.js
@@ -1,3 +1,25 @@
+var stateLocation = 0;
 $(document).on('click', '.btn', function(){
-  $('.box').toggleClass('active')
+  switch(stateLocation){
+    case 0:
+    if($('.box').hasClass('three')){
+      $('.box').removeClass('three')
+    }
+    stateLocation = 1;
+    break;
+    case 1:
+    $('.box').addClass('one')
+    stateLocation = 2;
+    break;
+    case 2:
+    $('.box').removeClass('one')
+    $('.box').addClass('two')
+    stateLocation = 3;
+    break;
+    case 3:
+    $('.box').removeClass('two')
+    $('.box').addClass('three')
+    stateLocation = 0;
+    break;
+  }
 })
